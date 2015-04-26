a792403f5c339055850caad2a8ee25b3e0fbba8b
Luke Swart <lukeswart@gmail.com>
2015-04-26 21:49:20 +0000

    Abstract 'production' and 'flavors' scripts in to common script

---
+++ docker-deploy-nginx.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# assumes that we have a "raingardens" container already deployed
+DOCKER_REPO="$1"
+echo "Docker repo: $DOCKER_REPO"
+
+
+if [[ "$1" = "lukeswart/duwamish-nginx-production" ]]; then
+    echo "killing container:"
+    docker kill nginx
+    echo "removing container:"
+    docker rm nginx
+
+    docker run -d \
+           --name "nginx" \
+           --volumes-from duwamish_flavor \
+           --link duwamish_flavor:duwamish_flavor \
+           -p 80:80 \
+           -it $DOCKER_REPO
+           # -it lukeswart/duwamish-nginx
+else # "lukeswart/duwamish-nginx-flavors"
+    echo "killing container:"
+    docker kill nginx-flavors
+    echo "removing container:"
+    docker rm nginx-flavors
+
+    docker run -d \
+           --name "nginx-flavors" \
+           --volumes-from raingardens \
+           --link raingardens:raingardens \
+           --link demo1:demo1 \
+           --link demo2:demo2 \
+           -p 80:80 \
+           -it $DOCKER_REPO
+
+fi
