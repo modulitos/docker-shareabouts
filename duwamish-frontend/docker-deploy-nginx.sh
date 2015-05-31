5b949a241f8bd864d9da2ba28020fe56edec282b
Luke Swart <lukeswart@gmail.com>
2015-05-31 03:39:23 +0000

    Add frontend deployment scripts

--- duwamish-frontend/docker-deploy-nginx.sh
+++ duwamish-frontend/docker-deploy-nginx.sh
@@ -5,37 +5,38 @@ DOCKER_REPO="$1"
 echo "Docker repo: $DOCKER_REPO"
 
 
-if [[ "$1" != "lukeswart/duwamish-nginx-flavors" ]]; then
+if [[ "$1" == "lukeswart/nginx-duwamish" ]]; then
     echo "killing container:"
-    docker kill nginx
+    docker kill nginx-duwamish
     echo "removing container:"
-    docker rm nginx
+    docker rm nginx-duwamish
 
     docker run -d \
-           --name "nginx" \
+           --name "nginx-duwamish" \
            --volumes-from duwamish_flavor \
            --link duwamish_flavor:duwamish_flavor \
            -p 80:80 \
-           -p 443:443 \
            -it $DOCKER_REPO
 
+#           -p 443:443 \
 #           -p 9090:9090 \
 #           --link duwamish_flavor:duwamish_flavor \
 #           --link geoserver:geoserver \
            # -it lukeswart/duwamish-nginx
-else # "lukeswart/duwamish-nginx-flavors"
+# else # "lukeswart/nginx-raingardens"
+elif [[ "$1" == "lukeswart/nginx-raingardens" ]]; then
     echo "killing container:"
-    docker kill nginx-flavors
+    docker kill nginx-raingardens
     echo "removing container:"
-    docker rm nginx-flavors
+    docker rm nginx-raingardens
 
     docker run -d \
-           --name "nginx-flavors" \
+           --name "nginx-raingardens" \
            --volumes-from raingardens \
            --link raingardens:raingardens \
-           --link demo1:demo1 \
-           --link demo2:demo2 \
            -p 80:80 \
            -it $DOCKER_REPO
+else
+    echo "docker repo not recognized: $DOCKER_REPO"
 
 fi
