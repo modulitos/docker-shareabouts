4cfd4b0b99a47e5b5b785bdfcfb47dad6e3dfab1
Luke Swart <lukeswart@gmail.com>
2015-11-01 03:05:11 +0000

    remove extra deploy script

--- frontend/compose-deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-source .env
-# echo "FLAVOR: ${FLAVOR}"
-# echo "DEBUG: ${DEBUG}"
-docker-compose -f compose.yml -p app up -d
