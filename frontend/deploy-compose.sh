7897665476cc383f7bd79e75c2dd14cb84a1d790
Luke Swart <lukeswart@gmail.com>
2016-01-16 03:24:30 -0800

    Use PROJECT_NAME variable for docker-compose deployment

--- frontend/deploy-compose.sh
+++ frontend/deploy-compose.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-docker rm -f `docker ps -aq -f name=frontend_*`
+docker rm -f `docker ps -aq -f name=${PROJECT_NAME}_*`
+# docker rm -f `docker ps -aq -f name=frontend_*`
 source .env
-export $(cat .env | grep ^[^#] | xargs) && cat ${COMPOSE_CONFIG} | envsubst | docker-compose -f - -p "frontend" up -d
+export $(cat .env | grep ^[^#] | xargs) && cat ${COMPOSE_CONFIG} | envsubst | docker-compose -f - -p ${PROJECT_NAME} up -d
