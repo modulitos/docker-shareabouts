8a7d8277c739a7e03948a1fd3c9244b8c72008f2
Luke Swart <luke@lukeswart.net>
2016-01-28 03:14:43 -0800

    deploy-compose updates for backend

--- backend/deploy-compose.sh
+++ backend/deploy-compose.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-docker rm -f `docker ps -aq -f name=${PROJECT_NAME}_*`
-# docker rm -f `docker ps -aq -f name=backend_*`
-source .env-new
-# export $(cat .env | grep ^[^#] | xargs) && cat ${COMPOSE_CONFIG} | envsubst | docker-compose -f - -p ${PROJECT_NAME} up -d
-export $(grep "^[^#]" .env | xargs) && cat ${COMPOSE_CONFIG} | envsubst | docker-compose -f - -p ${PROJECT_NAME} up -d
+docker rm -f `docker ps -aq -f name=backend_*`
+source .env
+export $(grep "^[^#]" .env | xargs) && cat backups-api-nginx_compose.yml | envsubst | docker-compose -f - -p backend_ up -d
