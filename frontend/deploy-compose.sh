f7d168b0d986b874e11e86d141cf3d001a17e47e
Luke Swart <lukeswart@gmail.com>
2016-02-19 10:48:12 +0000

    Refactor grep expression in frontend compose deployment script

--- frontend/deploy-compose.sh
+++ frontend/deploy-compose.sh
@@ -3,4 +3,4 @@
 docker rm -f `docker ps -aq -f name=${PROJECT_NAME}_*`
 # docker rm -f `docker ps -aq -f name=frontend_*`
 source .env
-export $(cat .env | grep ^[^#] | xargs) && cat ${COMPOSE_CONFIG} | envsubst | docker-compose -f - -p ${PROJECT_NAME} up -d
+export $(grep "^[^#]" .env | xargs) && cat ${COMPOSE_CONFIG} | envsubst | docker-compose -f - -p ${PROJECT_NAME} up -d
