1dd5ec0d5789ea3fd5632234376a5939399c0bb1
Luke Swart <luke@lukeswart.net>
2016-03-14 18:32:48 -0700

    Update deployments with compose version 2 and one compose/nginx file

--- frontend/deploy-compose.sh
+++ frontend/deploy-compose.sh
@@ -3,4 +3,10 @@
 docker rm -f `docker ps -aq -f name=${PROJECT_NAME}_*`
 # docker rm -f `docker ps -aq -f name=frontend_*`
 source .env
+
+# To avoid substituting nginx-related variables, lets specify only the
+# variables that we will substitute with envsubst:
+MYVARS='$FLAVOR_1:$FLAVOR_1_PORT:$FLAVOR_2:$FLAVOR_2_PORT:$FLAVOR_3:$FLAVOR_3_PORT'
+envsubst "$MYVARS" < nginx-template.conf > nginx.conf
+
 export $(grep "^[^#]" .env | xargs) && cat ${COMPOSE_CONFIG} | envsubst | docker-compose -f - -p ${PROJECT_NAME} up -d
