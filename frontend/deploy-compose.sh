8495d6fa31a033d3ff3767c7e7eb5704745c5c2c
Luke Swart <luke@lukeswart.net>
2016-03-17 14:56:31 -0700

    Rename nginx-template.conf -> nginx.conf

--- frontend/deploy-compose.sh
+++ frontend/deploy-compose.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 docker rm -f `docker ps -aq -f name=${PROJECT_NAME}_*`
-# docker rm -f `docker ps -aq -f name=frontend_*`
 source .env
 
 # To avoid substituting nginx-related variables, lets specify only the
 # variables that we will substitute with envsubst:
 MYVARS='$FLAVOR_1:$FLAVOR_1_PORT:$FLAVOR_2:$FLAVOR_2_PORT:$FLAVOR_3:$FLAVOR_3_PORT'
-envsubst "$MYVARS" < nginx-template.conf > nginx.conf
+envsubst "$MYVARS" < nginx.conf > nginx-envsubst.conf
 
 export $(grep "^[^#]" .env | xargs) && cat ${COMPOSE_CONFIG} | envsubst | docker-compose -f - -p ${PROJECT_NAME} up -d
