86fbf60c917afa671fe119e534cbc6e94a4086ac
Luke Swart <luke@lukeswart.net>
2016-03-19 01:28:48 -0700

    Minor update to MYVARS

--- frontend/deploy-compose.sh
+++ frontend/deploy-compose.sh
@@ -8,7 +8,7 @@ source .env
 
 # To avoid substituting nginx-related variables, lets specify only the
 # variables that we will substitute with envsubst:
-MYVARS='$FLAVOR_1:$FLAVOR_1_PORT:$FLAVOR_2:$FLAVOR_2_PORT:$FLAVOR_3:$FLAVOR_3_PORT:$FLAVOR_1_DOMAIN:$FLAVOR_2_DOMAIN:$FLAVOR_3_DOMAIN'
+MYVARS='$FLAVOR_1:$FLAVOR_1_PORT:$FLAVOR_1_DOMAIN:$FLAVOR_2:$FLAVOR_2_PORT:$FLAVOR_2_DOMAIN:$FLAVOR_3:$FLAVOR_3_PORT:$FLAVOR_3_DOMAIN'
 envsubst "$MYVARS" < nginx.conf > nginx-envsubst.conf
 
 cat ${COMPOSE_CONFIG} | envsubst | docker-compose -f - -p ${PROJECT_NAME} up -d
