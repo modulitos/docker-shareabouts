eb568613fb1ca62eca09890256871710e5dc66b0
Luke Swart <luke@lukeswart.net>
2016-03-19 10:34:16 -0700

    Update backend deployments to use new nginx template and image

--- backend/deploy-compose.sh
+++ backend/deploy-compose.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
 docker rm -f `docker ps -aq -f name=backend_*`
+
+# variables defined from now on to be automatically exported:
+set -a
 source .env
-export $(grep "^[^#]" .env | xargs) && cat backups-api-nginx_compose.yml | envsubst | docker-compose -f - -p backend_ up -d
+
+# To avoid substituting nginx-related variables, lets specify only the
+# variables that we will substitute with envsubst:
+NGINX_VARS='$DOMAINS'
+envsubst "$NGINX_VARS" < nginx.conf > nginx-envsubst.conf
+
+docker-compose -f compose.yml -p backend_ up -d
