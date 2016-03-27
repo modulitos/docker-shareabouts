8a2c0dad64d608ef5d46cccd82485bd24755fabd
Luke Swart <lukeswart@gmail.com>
2016-03-27 12:06:27 +0000

    Enable setting timezones of containers

---
+++ backend/set-timezone.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# This script sets the timezone of a docker container
+
+# Relocate the timezone file
+mkdir -p /config/etc && mv /etc/timezone /config/etc/ && ln -s /config/etc/timezone /etc/
+# Set timezone as specified in /config/etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
+# Set the time zone
+echo "$TZ" > /config/etc/timezone
+
