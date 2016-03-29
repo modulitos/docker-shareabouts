8b9a7bfce675865b2c3fa73607a8162e7a912d92
Luke Swart <lukeswart@gmail.com>
2016-03-29 06:13:26 +0000

    Update frontend to use letsencrypt and letsencrypt-cron

--- frontend/deploy-compose.sh
+++ frontend/deploy-compose.sh
@@ -8,9 +8,9 @@ source .env
 
 # To avoid substituting nginx-related variables, lets specify only the
 # variables that we will substitute with envsubst:
-NGINX_VARS='$FLAVOR_1:$FLAVOR_1_PORT:$FLAVOR_1_DOMAIN:$FLAVOR_2:$FLAVOR_2_PORT:$FLAVOR_2_DOMAIN:$FLAVOR_3:$FLAVOR_3_PORT:$FLAVOR_3_DOMAIN'
+NGINX_VARS='$FLAVOR_1:$FLAVOR_1_PORT:$FLAVOR_1_DOMAIN:$FLAVOR_2:$FLAVOR_2_PORT:$FLAVOR_2_DOMAIN:$FLAVOR_3:$FLAVOR_3_PORT:$FLAVOR_3_DOMAIN:$MY_DOMAIN_NAME:$DOMAINS'
 envsubst "$NGINX_VARS" < nginx.conf > nginx-envsubst.conf
 
 envsubst "$NGINX_VARS" < nginx-acme-challenge.conf > nginx-acme-challenge-envsubst.conf
 
-cat ${COMPOSE_CONFIG} | envsubst | docker-compose -f - -p ${PROJECT_NAME} up -d
+cat compose.yml | envsubst | docker-compose -f - -p ${PROJECT_NAME} up -d
