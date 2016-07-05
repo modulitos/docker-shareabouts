8477a4a4ab4215c45b612b71d889da8935087bed
Luke Swart <lukeswart@gmail.com>
2016-07-05 09:20:31 +0000

    Add new config with 9 flavors

--- frontend/letsdeploy.sh
+++ frontend/letsdeploy.sh
@@ -8,7 +8,7 @@ source .env
 
 # To avoid substituting nginx-related variables, lets specify only the
 # variables that we will substitute with envsubst:
-NGINX_VARS='$FLAVOR_1:$FLAVOR_1_PORT:$FLAVOR_1_DOMAIN:$FLAVOR_2:$FLAVOR_2_PORT:$FLAVOR_2_DOMAIN:$FLAVOR_3:$FLAVOR_3_PORT:$FLAVOR_3_DOMAIN:$MY_DOMAIN_NAME:$DOMAINS'
+NGINX_VARS='$FLAVOR_1:$FLAVOR_1_PORT:$FLAVOR_1_DOMAIN:$FLAVOR_2:$FLAVOR_2_PORT:$FLAVOR_2_DOMAIN:$FLAVOR_3:$FLAVOR_3_PORT:$FLAVOR_3_DOMAIN:$FLAVOR_4_PORT:$FLAVOR_4_DOMAIN:$FLAVOR_5_PORT:$FLAVOR_5_DOMAIN:$FLAVOR_6_PORT:$FLAVOR_6_DOMAIN:$FLAVOR_7_PORT:$FLAVOR_7_DOMAIN:$FLAVOR_8_PORT:$FLAVOR_8_DOMAIN:$FLAVOR_9_PORT:$FLAVOR_9_DOMAIN::$MY_DOMAIN_NAME:$DOMAINS'
 envsubst "$NGINX_VARS" < nginx.conf > nginx-envsubst.conf
 
 envsubst "$NGINX_VARS" < nginx-acme-challenge.conf > nginx-acme-challenge-envsubst.conf
