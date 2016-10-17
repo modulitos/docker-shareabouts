48e1c174027c665f4eefdd6a934ec87a553836d9
Luke Swart <luke@lukeswart.net>
2016-10-17 00:51:11 -0700

    Add pod1 config, add new flavors, minor pod0 updates

--- frontend/letsdeploy.sh
+++ frontend/letsdeploy.sh
@@ -10,8 +10,10 @@ source .env
 # variables that we will substitute with envsubst:
 NGINX_VARS='$MY_DOMAIN_NAME:$DOMAINS'
 envsubst "$NGINX_VARS" < pod0.conf > pod0-envsubst.conf
+envsubst "$NGINX_VARS" < pod1.conf > pod1-envsubst.conf
 
 # For SSL:
 envsubst "$NGINX_VARS" < nginx-acme-challenge.conf > nginx-acme-challenge-envsubst.conf
 
-docker-compose -f pod0.yml up -d
+# docker-compose -f pod0.yml up -d
+docker-compose -f pod1.yml up -d
