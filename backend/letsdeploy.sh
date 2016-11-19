8f779c0ce954fa40d5b6f57809486ccd2d875557
Luke Swart <luke@lukeswart.net>
2016-11-18 23:33:22 -0800

    refactor to use DEPLOYMENT_TYPE configuration

--- backend/letsdeploy.sh
+++ backend/letsdeploy.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-docker rm -f `docker ps -aq -f name=backend_*`
+docker rm -f `docker ps -aq -f name=${DEPLOYMENT_TYPE}_*`
 
 # variables defined from now on to be automatically exported:
 set -a
 source .env
 
-# To avoid substituting nginx-related variables, lets specify only the
-# variables that we will substitute with envsubst:
-NGINX_VARS='$DOMAINS:$MY_DOMAIN_NAME'
-envsubst "$NGINX_VARS" < nginx.conf > nginx-envsubst.conf
+if [[ $DEPLOYMENT_TYPE == 'production' ]]; then
+    # To avoid substituting nginx-related variables, lets specify only the
+    # variables that we will substitute with envsubst:
+    NGINX_VARS='$DOMAINS:$MY_DOMAIN_NAME'
+    envsubst "$NGINX_VARS" < nginx.conf > nginx-envsubst.conf
 
-envsubst "$NGINX_VARS" < nginx-acme-challenge.conf > nginx-acme-challenge-envsubst.conf
+    envsubst "$NGINX_VARS" < nginx-acme-challenge.conf > nginx-acme-challenge-envsubst.conf
+fi
 
-cat compose.yml | envsubst | docker-compose -f - -p backend_ up -d
+# We have the compose project (COMPOSE_PROJECT_NAME) and the deployment share the same name
+docker-compose -p ${DEPLOYMENT_TYPE} -f ${DEPLOYMENT_TYPE}.yml up -d
