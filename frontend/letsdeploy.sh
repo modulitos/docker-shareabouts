8b1615f7773145e2f7938f87de666d4171401631
Luke Swart <luke@lukeswart.net>
2017-01-15 17:31:15 -0800

    Add dev config setup

--- frontend/letsdeploy.sh
+++ frontend/letsdeploy.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-docker rm -f `docker ps -aq -f name=${COMPOSE_PROJECT_NAME}_*`
+echo "killing all project containers:"
+docker rm -f `docker ps -aq -f name=${PROJECT_NAME}_*`
 
 # variables defined from now on to be automatically exported:
 set -a
 source .env
 
+# If no args, just kill the containers.
+if [[ -z $1 ]]
+then
+  exit 0
+fi
+
+echo "starting project containers:"
 # To avoid substituting nginx-related variables, lets specify only the
 # variables that we will substitute with envsubst:
 NGINX_VARS='$MY_DOMAIN_NAME:$DOMAINS'
-envsubst "$NGINX_VARS" < pod0.conf > pod0-envsubst.conf
-envsubst "$NGINX_VARS" < pod1.conf > pod1-envsubst.conf
-envsubst "$NGINX_VARS" < raingardens.conf > raingardens-envsubst.conf
+envsubst "$NGINX_VARS" < ${1}.conf > ${1}-envsubst.conf
 
 # For SSL:
 envsubst "$NGINX_VARS" < nginx-acme-challenge.conf > nginx-acme-challenge-envsubst.conf
 
-# docker-compose -f pod0.yml up -d
-docker-compose -f pod1.yml up -d
+docker-compose -f ${1}.yml up -d
